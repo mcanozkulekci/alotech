484f1d43fc560a2c67b246c91f87db21ff5765e9
Mehmet Can <mehmetcanozkulekci@gmail.com>
2023-05-29 13:42:04 +0300

    API tokens are OK

--- alotech/settings.py
+++ alotech/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tracks',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,8 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        'APP_DIRS': True,
+
     },
 ]
 
--- tracks/views.py
+++ tracks/views.py
@@ -4,8 +4,18 @@ from django.shortcuts import render
 
 import json
 import random
+import spotipy
+import os
+from spotipy.oauth2 import SpotifyClientCredentials
 from django.http import JsonResponse
+from dotenv import load_dotenv
 
+load_dotenv()
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+
+print(client_id,client_secret)
 def get_random_artist(request, genre):
     # Load the genres data from the JSON file
     with open('alotech/genres.json') as file:
@@ -17,11 +27,40 @@ def get_random_artist(request, genre):
     # Pick a random artist from the list
     random_artist = random.choice(genre_artists) if genre_artists else None
 
-    response_data = {
-        'genre': genre,
-        'artist': random_artist
-    }
+    if random_artist:
+        # Initialize the Spotify API client
+        client_credentials_manager = SpotifyClientCredentials(client_id,client_secret)
+        spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    return JsonResponse(response_data)
+        # Search for the artist on Spotify
+        results = spotify.search(q=random_artist, type='artist', limit=1)
+        artists = results['artists']['items']
 
-    
+        if artists:
+            artist = artists[0]
+            artist_id = artist['id']
+
+            # Get the artist's top tracks
+            top_tracks = spotify.artist_top_tracks(artist_id=artist_id, country='US')
+
+            # Filter the most popular 10 tracks
+            popular_tracks = top_tracks['tracks'][:10]
+
+            # Prepare the response data
+            response_data = {
+                'artist': random_artist,
+                'tracks': []
+            }
+
+            for track in popular_tracks:
+                track_info = {
+                    'name': track['name'],
+                    'album': track['album']['name'],
+                    'preview_url': track['preview_url'],
+                    'image_url': track['album']['images'][0]['url']
+                }
+                response_data['tracks'].append(track_info)
+
+            return JsonResponse(response_data)
+
+    return JsonResponse({'error': 'No artist found for the given genre.'})
